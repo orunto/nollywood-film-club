6f744ca8ecf233fb01d132c2618a7fde50bd0832
orunto <oruntoeniola@aol.com>
2025-09-07 03:15:06 +0100

    fix: add min-height to body for improved layout stability

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${lexendDeca.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${lexendDeca.variable} antialiased min-h-screen`}
       ><StackProvider app={stackServerApp}><StackTheme>
         <QueryProvider>
           <Nav />
